9dae3913d62797c72d6aa33d5ac6dcedd24bc170
guiaugh <g.augustooliv200@gmail.com>
2024-05-20 20:42:50 -0300

    Lista crud de departamento

--- 13-api-empresas/src/routes/router.js
+++ 13-api-empresas/src/routes/router.js
@@ -1,18 +1,37 @@
 const express = require('express')
 const router = express.Router()
 
-const cargoController = require('../controllers/cargoController')
-const funcionarioController = require('../controllers/funcionarioController')
+// controllers
+const CargoController = require('../controllers/cargoController')
+const DepartamentoController = require('../controllers/departamentoController')
+const FuncionarioController = require('../controllers/funcionarioController')
 
+// validators
+const { validarId } = require('../validators/idValidacao')
 const { cargoValidador } = require('../validators/cargoValidacao')
+const { departamentoValidador } = require('../validators/departamentoValidacao')
+const { funcionarioValidador } = require('../validators/funcionarioValidacao')
 
-router.post('/cargos', cargoValidador, cargoController.create)
-router.get('/cargos', cargoController.getAll)
-router.get('/cargos/:id', cargoController.getById)
-router.put('/cargos/:id', cargoController.update)
-router.delete('/cargos/:id', cargoController.remove)
+// Cargos
+router.post('/cargos', cargoValidador, CargoController.create)
+router.get('/cargos', CargoController.getAll)
+router.get('/cargos/:id', validarId, CargoController.getById)
+router.put('/cargos/:id', validarId, cargoValidador, CargoController.update)
+router.delete('/cargos/:id', validarId, CargoController.remove)
 
-router.post('/funcionarios', funcionarioController.create)
-router.get('/funcionarios/:id', funcionarioController.getById)
+// Departamentos
+router.post('/departamentos', departamentoValidador, DepartamentoController.create)
+router.get('/departamentos', DepartamentoController.getAll)
+router.get('/departamentos/:id', validarId, DepartamentoController.getById)
+router.put('/departamentos/:id', validarId, departamentoValidador, DepartamentoController.update)
+router.delete('/departamentos/:id', validarId, DepartamentoController.remove)
+
+// Funcionarios
+router.post('/funcionarios', funcionarioValidador, FuncionarioController.create)
+router.get('/funcionarios', FuncionarioController.getAll)
+router.get('/funcionarios/:id', validarId, FuncionarioController.getById)
+router.put('/funcionarios/:id', validarId, funcionarioValidador, FuncionarioController.update)
+router.delete('/funcionarios/:id', validarId, FuncionarioController.remove)
+
 
 module.exports = router
